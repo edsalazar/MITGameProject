ab1f5bdba7a01b79f6b27418953f44e58be161ad
Esteban Salazar <Esteban@MacBook-Pro-de-Esteban.local>
2015-04-16 15:49:39 -0500

    First script created

---
+++ Assets/Scripts/TestScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Debug.Log ("Esteban creó este Script");
+	
+	}
+}
