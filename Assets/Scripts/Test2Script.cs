230b16af184af2bab4253f14ce576acc72b16493
Diego Adrada <diegoadrada@gmail.com>
2015-04-16 23:07:11 +0200

    Test Cambio Diego

--- Assets/Scripts/Test2Script.cs
+++ Assets/Scripts/Test2Script.cs
@@ -1,17 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class TestScript : MonoBehaviour {
+public class Test2Script : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
+		Debug.Log ("ESTE SCRIPT LO CREO DIEGO");
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-		Debug.Log ("Diego modificó este Script");
 	
 	}
 }
